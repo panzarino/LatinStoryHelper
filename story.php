a62b3889010a110bc27e7f83ee33406a941facc7
Zach Panzarino <zachary@panzarino.com>
2016-03-17 17:11:51 +0000

    Extract html from xml

--- story.php
+++ story.php
@@ -11,12 +11,22 @@ $content = array(); //Associative array representing what wiktionary returns
 foreach ($words as $word){ 
     if (!(array_key_exists($word, $content))){ //make sure key is not already in dictionary.
         $page = file_get_contents("https://en.wiktionary.org/w/api.php?format=xml&action=query&prop=extracts&titles=".$word."&redirects=true&continue"); //get the page from the wiktionary api
-        $content[$word] = $page; 
+        $content[$word] = $page;
     }
     
 }
 
+$parsed = array();
+
 foreach ($content as $word => $info){
-    //parse the array      
-    
+    //parse the array
+    $xml = simplexml_load_string($info);
+    $dom = new DOMDocument();
+    $dom->loadHTML($xml->query->pages->page->extract[0]);
+    $html = $dom->getElementsByTagName('span');
+}
+
+// output the data
+foreach ($words as $x){
+    echo $parsed[$x];
 }
