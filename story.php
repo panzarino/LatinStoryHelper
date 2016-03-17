04c2366f04521ae9f0972c77fcbaee63abca0c9e
Jake <jakesyl1998@gmail.com>
2016-03-17 14:25:01 +0000

    add comments

--- story.php
+++ story.php
@@ -6,17 +6,17 @@ $story = strip_tags($story);
 
 $words = explode(" ", $story);
 
-$content = array();
+$content = array(); //Associative array representing what wiktionary returns
 
-foreach ($words as $word){
-    if (!(array_key_exists($word, $content))){
-        $page = file_get_contents("https://en.wiktionary.org/w/api.php?format=xml&action=query&prop=extracts&titles=".$word."&redirects=true&continue");
-        $content[$word] = $page;
+foreach ($words as $word){ 
+    if (!(array_key_exists($word, $content))){ //make sure key is not already in dictionary.
+        $page = file_get_contents("https://en.wiktionary.org/w/api.php?format=xml&action=query&prop=extracts&titles=".$word."&redirects=true&continue"); //get the page from the wiktionary api
+        $content[$word] = $page; 
     }
     
 }
 
 
 foreach ($content as $x){
-        
+    //parse the array        
 }
