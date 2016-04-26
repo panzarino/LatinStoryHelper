a3ed90c5f8d7244003911229bebcc27b3f3835fc
Zach Panzarino <zachary@panzarino.com>
2016-04-26 22:44:29 +0000

    Better dictionary results

--- js/main.js
+++ js/main.js
@@ -14,18 +14,13 @@ $("#searchform").submit(function(event) {
     });
 });
 
-$("#dictionary-form").submit(function(event) {
-    // stop form from submitting normally
-    event.preventDefault();
-    // display loading icon
-    $("body").addClass("loading");
+$("#dictionary-search").keyup(function() {
     // get form data
     var searchdata = $("#dictionary-form").serialize();
     var getting = $.get("dictionary.php", searchdata);
         /* Alerts the results */
     getting.done(function(data){
         // replace page content
-        $("body").removeClass("loading");
         $("#dictionary-output").html(data);
     });
 });
