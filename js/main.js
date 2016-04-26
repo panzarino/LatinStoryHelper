70cd741cb6bc60e62db7a386e3cda61e499de5a1
Zach Panzarino <zachary@panzarino.com>
2016-04-26 15:21:22 +0000

    Ajax dictionary

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ $("#searchform").submit(function(event) {
         $("#output").html(data);
     });
 });
+
+$("#dictionary-form").submit(function(event) {
+    // stop form from submitting normally
+    event.preventDefault();
+    // display loading icon
+    $("body").addClass("loading");
+    // get form data
+    var searchdata = $("#dictionary-form").serialize();
+    var getting = $.get("dictionary.php", searchdata);
+        /* Alerts the results */
+    getting.done(function(data){
+        // replace page content
+        $("body").removeClass("loading");
+        $("#dictionary-output").text(data);
+    });
+});
