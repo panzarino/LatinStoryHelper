c86d0cdc8c8b7176c33428718ec044a4f8431bdc
Zach Panzarino <zachary@panzarino.com>
2016-05-03 20:38:38 -0400

    Fix highlighting issues
    More specific way to distinguish between infinitives and main verbs

--- story.php
+++ story.php
@@ -38,7 +38,7 @@ foreach ($content as $word => $info){
             if (strpos($compare, 'participle')!=false){
                 $highlight[$word] = "yellow";
             }
-            elseif (strpos($compare, 'infinitive')!=false && !strpos($compare, 'pronunciation')){
+            elseif (strpos($compare, 'infinitive')!=false && !strpos($compare, 'conjugation')){
                 $highlight[$word] = "green";
             }
             elseif (strpos($compare, 'subjunctive')!=false){
